9ac54bb7c3989b25d926ac21eb2d9fa9d610a8a9
Yewbdar <Yewbdar.grima@gmail.com>
2018-04-25 08:04:13 -0500

    json file added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,17 @@ var path = require("path");
 var friends=require("../data/friends");
 var fs=require("fs");
 
-
+module.exports = function (app){
  app.get("/api/friends",function(req,res){
-     console.log("am here");
-    //var data = fs.readFile( "../data/friends.js","utf8",function(err,res){
-   var word=JSON.parse(req.friends);
-   
-    console.log(word);
-  //});
+     res.json(friends);
+    
  });
  
   app.post("/api/friends",function(){
 
   });
-
-  console.log(friends[0].scores);
-
+}
+  
     
 
  
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app=express();
-var PORT=3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
 
 app.listen(PORT,function(){
     console.log("server start....",PORT);
-    var htmlRoites=require("./app/routing/htmlRoutes");
-    var apiRoutes=require("./app/routing/apiRoutes");
+   
 })
-module.exports=app;
+// module.exports=app;
